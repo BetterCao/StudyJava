33c0c77c8f8b9d16b1d9b01c32f49dae63481f99
caojuepeng <lx-cao.juepeng@sfit.shfe.com.cn>
2018-07-19 16:22:12 +0800

    类的继承，展示子类和父类的关系

---
+++ StudyExercise/TestSon.java
@@ -0,0 +1,15 @@
+/*
+ *类的继承
+ *展示子类和父类的关系（继承知识点，运行这一个就可以）
+ *作者：曹机智
+ *创建时间：2018.07.19
+ */
+
+public class TestSon{    //展示父类、子类的关系
+	public static void main(String args[]){
+		Son x = new Son();
+		System.out.println("lalala");
+		Son y = new Son(2);	
+		y.whichEat();
+	}
+}
