3a5c135d5893d0ce74823327b363944bf75e0e6b
caojuepeng <lx-cao.juepeng@sfit.shfe.com.cn>
2018-07-30 15:51:00 +0800

    冒泡排序

---
+++ Examples/BubbleSort.java
@@ -0,0 +1,25 @@
+/*
+ *冒泡排序
+ *作者：曹珏朋
+ *创建时间：2018.07.30
+ */
+
+public class BubbleSort{
+	public static void main(String args[]){
+		int a[] = {23,3,5,77,8,9,1};
+		int len = a.length;
+		int t;
+		for(int i=0;i<len-1;i++){	//轮数，len - 1
+			for(int j=0;j<len-1;j++){	//每一轮，相邻的数两两比较，最大的排到最后
+				if(a[j]>a[j+1]){
+					t = a[j];
+					a[j] = a[j+1];
+					a[j+1] = t;
+				}
+			}
+		}
+		for(int i=0;i<len;i++){
+			System.out.println(a[i]);
+		}
+	}
+}
