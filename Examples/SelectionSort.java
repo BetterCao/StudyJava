0de7da9f3f4dd15b95c9ebe38f5ce7e4b5a7859e
caojuepeng <lx-cao.juepeng@sfit.shfe.com.cn>
2018-07-30 15:51:12 +0800

    选择排序

---
+++ Examples/SelectionSort.java
@@ -0,0 +1,29 @@
+/*
+ *选择排序
+ *作者：曹机智
+ *创建时间：2018.07.30
+ */
+
+public class SelectionSort{
+	public static void main(String args[]){
+		int a[] = {23,3,5,77,8,9,1};
+		int len = a.length;
+		int t;
+		for(int i=0;i<len-1;i++){
+			int min = i;	//最小标志
+			for(int j=i+1;j<len;j++){
+				if(a[j]<a[min]){	//同一轮中，比较两个数，找到较小的，和其他数继续比，将这一轮中最小数的下标设为最小标志
+					min = j;
+				}
+			}
+			if(min!=i){
+				t = a[min];
+				a[min] = a[i];
+				a[i] = t;
+			}
+		}
+		for(int i=0;i<len;i++){
+			System.out.println(a[i]);
+		}
+	}
+}
